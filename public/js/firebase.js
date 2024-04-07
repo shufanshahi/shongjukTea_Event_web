c1ea46032e4b93757d0ef4ffe193c6fa920b26ce
Shufan Shahi <shufanshahi@gmail.com>
2024-04-07 21:40:23 +0600

    blog web upload

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDREJm9y7GUb5roFlybPt0bvN_j9SckXfc",
+    authDomain: "event-website-c46c7.firebaseapp.com",
+    projectId: "event-website-c46c7",
+    storageBucket: "event-website-c46c7.appspot.com",
+    messagingSenderId: "85127683004",
+    appId: "1:85127683004:web:de41f28c926570782fa9b9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
